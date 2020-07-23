e9e15eee2fb4cfd0ac0ee4f50488ce6ba778b9c9
Javier Fernández Barreiro <47840319+barrrettt@users.noreply.github.com>
2020-07-23 08:03:14 +0200

    Simplify login activity

---
+++ app/src/main/java/com/barrrettt/androidjwtrestapiexample/data/user/User.java
@@ -0,0 +1,16 @@
+package com.barrrettt.androidjwtrestapiexample.data.user;
+
+/**
+ * Usuario para logIn
+ */
+public class User {
+
+    private String name;
+    private String jwt;
+
+    public User(String name, String jwt) {
+        this.name = name;
+        this.name = name;
+    }
+
+}
